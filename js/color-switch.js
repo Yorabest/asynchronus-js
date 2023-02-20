c32a5b06df79168e53629ea357a68b61b0c564f1
Sophia <melnichenko.cofia07@gmail.com>
2023-02-20 16:21:36 +0200

    50

---
+++ js/color-switch.js
@@ -0,0 +1,34 @@
+const bodyRef = document.querySelector('body');
+const startBtnRef = document.querySelector('[data-action="start"]');
+const stopBtnRef = document.querySelector('[data-action="stop"]');
+
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+startBtnRef.addEventListener('click',handleStart);
+stopBtnRef.addEventListener('click',handleStop);
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+const start = () => {
+    bodyRef.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+    startBtnRef.disabled = true;
+};
+let colorSwitchId = null;
+
+function handleStart() {
+   return colorSwitchId = setInterval(start, 1000);
+    
+};
+
+function handleStop() {
+    startBtnRef.disabled = false;
+    return clearInterval(colorSwitchId);
+}
